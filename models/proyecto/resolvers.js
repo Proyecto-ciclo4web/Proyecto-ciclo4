123517a414ac7dd6f6cf3f207927e84ac98df431
laurock91 <90356756+laurock91@users.noreply.github.com>
2021-11-19 09:56:57 -0500

    creacion de historias

---
+++ models/proyecto/resolvers.js
@@ -0,0 +1,37 @@
+import { ProjectModel } from './proyecto.js';
+
+const resolversProyecto = {
+  Query: {
+    Proyectos: async (parent, args) => {
+      const proyectos = await ProjectModel.find().populate('avances').populate('inscripciones');
+      return proyectos;
+    },
+  },
+  Mutation: {
+    crearProyecto: async (parent, args) => {
+      const proyectoCreado = await ProjectModel.create({
+        nombre: args.nombre,
+        estado: args.estado,
+        fase: args.fase,
+        fechaInicio: args.fechaInicio,
+        fechaFin: args.fechaFin,
+        presupuesto: args.presupuesto,
+        lider: args.lider,
+        objetivos: args.objetivos,
+      });
+      return proyectoCreado;
+    },
+
+    CambiarEstadoProyecto: async (parent, args) => {
+      const CambiarEstado =await ProjectModel.findByIdAndUpdate({_id : args.id},{estado:args.estado})
+
+  
+    },
+    CambiarFaseProyecto: async (parent, args) => {
+      const CambiarFase =await ProjectModel.findByIdAndUpdate({_id : args.id},{fase:args.fase})
+    }
+  },
+
+};
+
+export { resolversProyecto };
---
+++ models/proyecto/tipos.js
@@ -0,0 +1,65 @@
+import { gql } from 'apollo-server-express';
+
+const tiposProyecto = gql`
+  type Objetivo {
+    _id: ID!
+    descripcion: String!
+    tipo: Enum_TipoObjetivo!
+  }
+
+  input crearObjetivo {
+    descripcion: String!
+    tipo: Enum_TipoObjetivo!
+  }
+
+  type Proyecto {
+    _id: ID!
+    nombre: String!
+    presupuesto: Float!
+    fechaInicio: Date!
+    fechaFin: Date!
+    estado: Enum_EstadoProyecto!
+    fase: Enum_FaseProyecto!
+    lider: Usuario!
+    objetivos: [Objetivo]
+    avances: [Avance]
+    inscripciones: [Inscripcion]
+  }
+
+  type Query {
+    Proyectos: [Proyecto]
+  }
+
+  type Mutation {
+    crearProyecto(
+      nombre: String!
+      presupuesto: Float!
+      fechaInicio: Date!
+      fechaFin: Date!
+      estado: Enum_EstadoProyecto!
+      fase: Enum_FaseProyecto!
+      lider: String!
+      objetivos: [crearObjetivo]
+    ): Proyecto
+
+  CambiarEstadoProyecto (            
+  id:ID!
+  estado:Enum_EstadoProyecto!) : Proyecto
+
+  CambiarFaseProyecto (
+  id:ID!
+  fase:Enum_FaseProyecto!) : Proyecto
+
+  
+
+
+
+
+  }
+  
+
+
+
+`;
+
+export { tiposProyecto };
